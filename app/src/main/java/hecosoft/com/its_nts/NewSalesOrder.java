6feac2e144f90c663cb57becf6dce2a4453a71c7
rAnA aRsl <awesum.rana@gmail.com>
2017-06-15 16:50:23 +0500

    spinnercode

--- app/src/main/java/hecosoft/com/its_nts/NewSalesOrder.java
+++ app/src/main/java/hecosoft/com/its_nts/NewSalesOrder.java
@@ -2,12 +2,26 @@ package hecosoft.com.its_nts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class NewSalesOrder extends AppCompatActivity {
-
+AutoCompleteTextView name;
+    EditText id;
+    Spinner ship;
+    String[] country = {"Pakistan", "India", "Uk", "China"};
+    ArrayAdapter<String> countryListAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_sales_order);
+    name=(AutoCompleteTextView)findViewById(R.id.nameautocmplete);
+        id=(EditText)findViewById(R.id.showid);
+        ship=(Spinner)findViewById(R.id.spinnershipto);
+        countryListAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, country);
+
+        ship.setAdapter(countryListAdapter);
     }
 }
