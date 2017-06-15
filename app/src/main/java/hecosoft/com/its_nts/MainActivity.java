bf703f233431e262747bf1ba3cea6ad31cd0a910
rAnA aRsl <awesum.rana@gmail.com>
2017-06-15 16:44:05 +0500

    layoutChecking

--- app/src/main/java/hecosoft/com/its_nts/MainActivity.java
+++ app/src/main/java/hecosoft/com/its_nts/MainActivity.java
@@ -1,16 +1,32 @@
 package hecosoft.com.its_nts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btn1,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hello
-        //hello
-        //hello
+        btn1=(Button)findViewById(R.id.arslan);
+        btn2=(Button)findViewById(R.id.fareed);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,EmpSchedule.class);
+                startActivity(i);
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,NewSalesOrder.class);
+            startActivity(i);
+            }
+        });
     }
 }
