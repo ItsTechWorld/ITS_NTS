4f01b25905b6951738f2b9b41d1fa11ef17b66cd
rAnA aRsl <awesum.rana@gmail.com>
2017-06-15 15:56:54 +0500

    NewSalesOrderActivity

--- app/src/main/java/hecosoft/com/its_nts/Check.java
+++ app/src/main/java/hecosoft/com/its_nts/Check.java
@@ -4,6 +4,5 @@ package hecosoft.com.its_nts;
  * Created by rAnA aRsI on 6/15/2017.
  */
 public class Check {
-    //hello
-    //final checkong from my side
+
 }
---
+++ app/src/main/java/hecosoft/com/its_nts/NewSalesOrder.java
@@ -0,0 +1,13 @@
+package hecosoft.com.its_nts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewSalesOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_sales_order);
+    }
+}
