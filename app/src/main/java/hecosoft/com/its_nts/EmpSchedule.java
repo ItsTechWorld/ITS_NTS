8a81bad6ff63e3f27809fd7ec077f8a2497dec54
faree <fareeddaar5@gmail.com>
2017-06-15 15:53:42 +0500

    fareed here

---
+++ app/src/main/java/hecosoft/com/its_nts/EmpSchedule.java
@@ -0,0 +1,13 @@
+package hecosoft.com.its_nts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmpSchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emp_schedule);
+    }
+}
