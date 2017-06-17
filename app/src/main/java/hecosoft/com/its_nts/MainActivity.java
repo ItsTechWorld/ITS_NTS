49c05f47a945445118ca7b5a55fdb0680ccdfb14
rAnA aRsl <awesum.rana@gmail.com>
2017-06-17 19:58:39 +0500

    LoginFormAddition

--- app/src/main/java/hecosoft/com/its_nts/MainActivity.java
+++ app/src/main/java/hecosoft/com/its_nts/MainActivity.java
@@ -5,28 +5,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 Button btn1,btn2;
+    EditText uname,upass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn1=(Button)findViewById(R.id.arslan);
-        btn2=(Button)findViewById(R.id.fareed);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(MainActivity.this,EmpSchedule.class);
-                startActivity(i);
-            }
-        });
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(MainActivity.this,NewSalesOrder.class);
-            startActivity(i);
-            }
-        });
+        btn1=(Button)findViewById(R.id.login);
+        btn2=(Button)findViewById(R.id.register);
+uname=(EditText)findViewById(R.id.uname);
+        upass=(EditText)findViewById(R.id.upass);
+       btn1.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               if(uname.getText().toString().equals("arsi")&&upass.getText().toString().equals("arsi"))
+               {
+                   Intent i=new Intent(getApplicationContext(),NewSalesOrder.class);
+                   i.putExtra("uname",uname.getText().toString());
+                   startActivity(i);
+               }
+           }
+       });
     }
+
+
 }
