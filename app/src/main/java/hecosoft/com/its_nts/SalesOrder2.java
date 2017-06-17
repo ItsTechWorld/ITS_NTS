6d76690cdd4c0e208c32c106d2b65e0c39c06756
rAnA aRsl <awesum.rana@gmail.com>
2017-06-17 13:28:01 +0500

    layout final

---
+++ app/src/main/java/hecosoft/com/its_nts/SalesOrder2.java
@@ -0,0 +1,13 @@
+package hecosoft.com.its_nts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SalesOrder2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales_order2);
+    }
+}
